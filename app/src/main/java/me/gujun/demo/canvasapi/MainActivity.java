e93f34e5aacdb7ac5fb27b25d73c48d09ffadb51
Jun Gu <2dxgujun@gmail.com>
2015-01-27 16:06:40 +0800

    Initial commit

---
+++ app/src/main/java/me/gujun/demo/canvasapi/MainActivity.java
@@ -0,0 +1,13 @@
+package me.gujun.demo.canvasapi;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
